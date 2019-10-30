4edb17a76d12e09330bd483ba717472c29562060
wolfene2001 <wolfene2001@yahoo.com>
2019-10-30 11:28:57 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,11 +4,10 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    block_return_values <<
-    yield(collection[i])
+    block_return_values << yield(collection[i])
     i = i + 1
   end
-end
+
 
 if block_return_values.include?(false)
     false
